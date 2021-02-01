650ed576cb9cb30c9832ec8300c28fd4adf5ea52
Rasika Gayan Gunarathna <rasikagayan90@gmail.com>
2021-02-01 22:33:15 +0800

    express code

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,29 @@
 import { MikroORM } from "@mikro-orm/core";
 import { Post } from "./entities/Post";
 import microConfig from "./mikro-orm.config";
+import express from "express";
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
   // run migration
   await orm.getMigrator().up();
 
-  const post = orm.em.create(Post, {
-    title: "Let's learn react",
+  const port = 4001;
+
+  const app = express();
+
+  app.get("/", (req, res) => {
+    res.json({ hello: "world" });
   });
-  await orm.em.persistAndFlush(post);
+
+  app.listen(port, () => {
+    console.log(`Server Start to Listen Port ${port}`);
+  });
+
+  // const post = orm.em.create(Post, {
+  //   title: "Let's learn react",
+  // });
+  // await orm.em.persistAndFlush(post);
 };
 
 main().catch((err) => console.log(err));
